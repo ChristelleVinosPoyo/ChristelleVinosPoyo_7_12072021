83ed62d540148d9a3d00d118ed8b48f07cdb5133
Christelle <christellevinospoyo@gmail.com>
2021-07-19 18:21:40 +0200

    route modifyPost OK

--- backend/app.js
+++ backend/app.js
@@ -59,71 +59,93 @@ app.get('/api/posts', (req, res, next) => {
   })
 });
 
+// GetUser
+app.get('/api/users/:id', (req, res, next) => {
+  db.query(`SELECT * FROM users WHERE id = ${req.params.id}` , (err, data) => {
+    if (err) { return res.status(400).send({ message: "une erreur est survenue !" }) };
+    res.send(data);
+    console.log(data);
+  })
+});
+
 // CreatePost - fonctionne
 app.post('/api/posts', (req, res, next) => {
   console.log(req.body.post);
   db.query(`INSERT INTO posts (id, post, user_id, data_of_post) VALUES (NULL, ${mysql.escape(req.body.post)}, '${req.body.user_id}', CURRENT_TIMESTAMP)  `, 
-    (err, data) => {
-      if (err) { return res.status(400).json({ err }) };
-      console.log(err);
-      res.status(200).json({ message: 'Votre message a été posté !'});
+  (err, data) => {
+    if (err) { return res.status(400).json({ err }) };
+    console.log(err);
+    res.status(200).json({ message: 'Votre message a été posté !'});
   });
 });
+
 
 //signup - Fonctionne 
 app.post('/api/users/signup', (req, res, next) =>{
   bcrypt.hash(req.body.password, 10) // 10 tours d'execution de l'algorythme de hashage
-    .then(hash => {
-      console.log(hash);
-      db.query(`INSERT INTO users (id, firstname, lastname, email, password, age, picture) VALUES (NULL, '${req.body.firstname}', '${req.body.lastname}', '${req.body.email}', '${hash}', ${req.body.age}, '${req.body.picture}')`, (err, data) => {
-        if (err) { return res.status(400).json({ err }) };
-        res.status(200).json({ message: 'Votre compte a bien été créé !'});
-      }
-      )
-    })
+  .then(hash => {
+    console.log(hash);
+    db.query(`INSERT INTO users (id, firstname, lastname, email, password, age, picture) VALUES (NULL, '${req.body.firstname}', '${req.body.lastname}', '${req.body.email}', '${hash}', ${req.body.age}, '${req.body.picture}')`, (err, data) => {
+      if (err) { return res.status(400).json({ err }) };
+      res.status(200).json({ message: 'Votre compte a bien été créé !'});
+    }
+    )
+  })
 })
 
-//login - ne fonctionne pas - revoir le format json de data
+//login - ne fonctionne pas - pb avec jsonwebtoken
 app.post('/api/users/login', (req, res, next) => {
   // rechercher du hash dans la BDD
-  db.query(`SELECT password FROM users WHERE email='${req.body.email}'`, (err, data) => {
+  db.query(`SELECT * FROM users WHERE email='${req.body.email}'`, (err, data) => {
     if (err) { return res.status(400).send({ message: "utilisateur non trouvé !" }) };
     console.log(data);
-    const hash_db = data;
-    bcrypt.compare(req.body.password, JSON.parse(hash_db))
-      .then(valid => {
-        console.log(valid);
-        if (!valid) {
-          return res.status(401).json({ error: 'Mot de passe incorrect ! ' })
-        }
-        res.status(200).send({ message: "utilisateur connecté !" })
-        // res.status(200).json({ 
-        //   userId: user._id,
-        //   token: jwt.sign( // le token sécurise l'échange des données : il valide le droit de l'utilisateur à accéder aux ressources
-        //       { userId: user._id }, // 1er argument : ce qu'on veut encoder
-        //       process.env.TOKEN_KEY, // 2eme argument : clé secrète d'encodage
-        //       { expiresIn: '24h' } // 3ème argument : chaque token durera 24h
-        //   )
-      });
+    bcrypt.compare(req.body.password, data[0].password)
+    .then(valid => {
+      console.log(valid);
+      if (!valid) {
+        return res.status(401).json({ error: 'Mot de passe incorrect ! ' })
+      }
+      // res.status(200).send({ message: "utilisateur connecté !" })
+
+      res.status(200).json({ 
+          userId: data[0].user_id,
+          token: jwt.sign( // le token sécurise l'échange des données : il valide le droit de l'utilisateur à accéder aux ressources
+              { userId: data[0].user_id }, // 1er argument : ce qu'on veut encoder
+              process.env.TOKEN_KEY, // 2eme argument : clé secrète d'encodage
+              { expiresIn: '24h' } // 3ème argument : chaque token durera 24h
+          )
       })
-  });
-
-//modifyPost
-
-
-// deletePost - à revoir
-app.delete('/api/posts/:id', (req, res, next) => {
-  console.log(req.params.id);
-  db.query(`DELETE FROM posts WHERE id = '${req.params.id}'`, (err, data) => {
-    if (err) { return res.status(400).send({ message: "une erreur est survenue !" }) };
-    res.status(200).json({ message: 'Votre message a été supprimé !'});
+    });
   })
 });
-
-// deleteUser - à revoir
+  
+  //modifyPost
+  app.put('/api/posts/:id', (req, res, next) => {
+    db.query(`SELECT user_id FROM posts WHERE id = ${req.params.id}`, (err, data) => {
+      if (err) { return res.status(400).send({ message: "une erreur est survenue !" }) };
+      if (data[0].user_id != req.body.userId) {
+        res.status(400).send({ message: "modification impossible car ce message appartient à un autre utilisateur." })
+      };
+      if (data[0].user_id == req.body.userId) {
+        db.query(`UPDATE posts SET post = '${req.body.post}' WHERE id = ${req.params.id}`, (err, data) => {
+          if (err) { return res.status(400).send({ message: "une erreur est survenue !" }) };
+          res.status(200).json({ message: 'Votre message a été modifié !'});
+        })
+      }
+    })
+  });
+  
+  // deletePost - fonctionne
+  app.delete('/api/posts/:id', (req, res, next) => {
+    db.query(`DELETE FROM posts WHERE id = ${req.params.id}`, (err, data) => {
+      if (err) { return res.status(400).send({ message: "une erreur est survenue !" }) };
+      res.status(200).json({ message: 'Votre message a été supprimé !'});
+    })
+  });
+  
+// deleteUser - fonctionne
 app.delete('/api/users/:id', (req, res, next) => {
-  console.log(req.params.id);
-  db.query(`DELETE FROM users WHERE id = '${req.params.id}'`, (err, data) => {
+  db.query(`DELETE FROM users WHERE id = ${req.params.id}`, (err, data) => {
     if (err) { return res.status(400).send({ message: "une erreur est survenue !" }) };
     res.status(200).json({ message: 'Votre compte a été supprimé !'});
   })
