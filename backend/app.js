87b1a47eaab7b9ff62f69ae04f29a8d7a645e183
Christelle <christellevinospoyo@gmail.com>
2021-07-19 15:33:38 +0200

    getAllPost, signu,, createPost OK

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 require('dotenv').config();
+const bcrypt = require('bcrypt');
+
 // const path = require('path');
 // const helmet = require('helmet');
 // const rateLimit = require("express-rate-limit"); // pour limiter le nombres d'appels faits à l'API
@@ -48,16 +50,8 @@ app.use(express.json()); // pour parlier à l'utilisation de bodyparser qui est 
 // routes vers les ressources de la BD
 // app.use('/api/users', userRoutes);
 
-// getAllUsers - a supprimer-  mais fonctionne
-app.get('/api/users', (req, res, next) => {
-  db.query("SELECT * FROM users", (err, data) => {
-    if (err) { return res.status(400).send({ message: "une erreur est survenue !" }) };
-    res.send(data);
-  })
-});
 
-// postRoutes
-// GetAllPosts - fonctionne
+// GetAllPost - fonctionne
 app.get('/api/posts', (req, res, next) => {
   db.query("SELECT * FROM posts", (err, data) => {
     if (err) { return res.status(400).send({ message: "une erreur est survenue !" }) };
@@ -65,32 +59,63 @@ app.get('/api/posts', (req, res, next) => {
   })
 });
 
-// CreatePost - EN COURS
+// CreatePost - fonctionne
 app.post('/api/posts', (req, res, next) => {
   console.log(req.body.post);
-  db.query(`INSERT INTO posts VALUES (NULL, ${req.body.post}, ${req.body.user_id}, CURRENT_TIMESTAMP) `, 
+  db.query(`INSERT INTO posts (id, post, user_id, data_of_post) VALUES (NULL, ${mysql.escape(req.body.post)}, '${req.body.user_id}', CURRENT_TIMESTAMP)  `, 
     (err, data) => {
-      if (err) { return res.status(400).send({ message: "une erreur est survenue !" }) };
+      if (err) { return res.status(400).json({ err }) };
+      console.log(err);
       res.status(200).json({ message: 'Votre message a été posté !'});
   });
 });
 
-//signup - à revoir
-const bcrypt = require('bcrypt');
+//signup - Fonctionne 
 app.post('/api/users/signup', (req, res, next) =>{
   bcrypt.hash(req.body.password, 10) // 10 tours d'execution de l'algorythme de hashage
     .then(hash => {
-      db.query(`INSERT INTO users VALUES (NULL, ${req.body.firstname}, ${req.body.lastname}, ${req.body.email}, ${hash}, ${req.body.age}, ${req.body.picture})`, (err, data) => {
-        if (err) { return res.status(400).send({ message: "une erreur est survenue !" }) };
+      console.log(hash);
+      db.query(`INSERT INTO users (id, firstname, lastname, email, password, age, picture) VALUES (NULL, '${req.body.firstname}', '${req.body.lastname}', '${req.body.email}', '${hash}', ${req.body.age}, '${req.body.picture}')`, (err, data) => {
+        if (err) { return res.status(400).json({ err }) };
         res.status(200).json({ message: 'Votre compte a bien été créé !'});
       }
       )
     })
 })
-// deleteOnePost - à revoir
-app.delete('.api/posts/:id', (req, res, next) => {
+
+//login - ne fonctionne pas - revoir le format json de data
+app.post('/api/users/login', (req, res, next) => {
+  // rechercher du hash dans la BDD
+  db.query(`SELECT password FROM users WHERE email='${req.body.email}'`, (err, data) => {
+    if (err) { return res.status(400).send({ message: "utilisateur non trouvé !" }) };
+    console.log(data);
+    const hash_db = data;
+    bcrypt.compare(req.body.password, JSON.parse(hash_db))
+      .then(valid => {
+        console.log(valid);
+        if (!valid) {
+          return res.status(401).json({ error: 'Mot de passe incorrect ! ' })
+        }
+        res.status(200).send({ message: "utilisateur connecté !" })
+        // res.status(200).json({ 
+        //   userId: user._id,
+        //   token: jwt.sign( // le token sécurise l'échange des données : il valide le droit de l'utilisateur à accéder aux ressources
+        //       { userId: user._id }, // 1er argument : ce qu'on veut encoder
+        //       process.env.TOKEN_KEY, // 2eme argument : clé secrète d'encodage
+        //       { expiresIn: '24h' } // 3ème argument : chaque token durera 24h
+        //   )
+      });
+      })
+  });
+
+//modifyPost
+
+
+// deletePost - à revoir
+app.delete('/api/posts/:id', (req, res, next) => {
   const postId = req.params.id;
-  db.query(`DELETE * FROM posts WHERE id = ${postId}`, (err, data) => {
+  console.log(postId);
+  db.query(`DELETE FROM posts WHERE id = ${postId}`, (err, data) => {
     if (err) { return res.status(400).send({ message: "une erreur est survenue !" }) };
     res.status(200).json({ message: 'Votre message a été supprimé !'});
   })
