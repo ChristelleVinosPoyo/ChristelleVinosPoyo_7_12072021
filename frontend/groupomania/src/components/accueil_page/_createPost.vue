de428822cdd5046504662fb7599a60ce5fedc802
Christelle <christellevinospoyo@gmail.com>
2021-07-28 16:33:41 +0200

    commit befor vuex install

--- frontend/groupomania/src/components/accueil_page/_createPost.vue
+++ frontend/groupomania/src/components/accueil_page/_createPost.vue
@@ -2,9 +2,9 @@
     <div class="create-post">
         <form action="">
             <p>
-                <input type="text" name="Nom" id="createpost" class="input zone-text" placeholder="Ecris ton message ici !">
+                <textarea v-model="formData.post" name="createpost" placeholder="Ecris ton message ici !"></textarea>
             </p>
-            <button type="submit">
+            <button v-on:click.prevent="createPostFct" type="submit">
                 Envoyer
             </button>
         </form>
@@ -13,8 +13,36 @@
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
-        name: 'CreatePost'
+        name: 'CreatePost',
+                data(){
+            return {
+                formData: {
+                    userId: '',
+                    post: '',
+                }
+            }
+        },
+        methods: {
+            createPostFct: function() {
+                console.log(this.formData);
+                axios
+                .post('http://localhost:3000/api/posts', {
+                    userId: this.formData.userId,
+                    post: this.formData.post
+                })
+                .then(res => {
+                    console.log(res.data.token);
+                    alert("Message envoyé !");
+                })
+                .catch(error => {
+                console.log(error);
+                alert("Une erreur est survenue !");
+          })
+            }
+        }
+
     }
 </script>
 
@@ -25,5 +53,9 @@
         margin-bottom: 15px;
         background-color: #091f43;
         border-radius: 5px;
+    }
+    textarea {
+        width: 90%;
+        height: 70px;
     }
 </style>
